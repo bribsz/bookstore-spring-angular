10550232a83e962f0dd790ab6cfd6c16dd86fa65
bribsz <brenohribeiroh@gmail.com>
2023-07-28 18:59:05 -0300

    create and update

--- src/main/java/com/bribsz/bookstore/controllers/CategoriaController.java
+++ src/main/java/com/bribsz/bookstore/controllers/CategoriaController.java
@@ -1,5 +1,6 @@
 package com.bribsz.bookstore.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.bribsz.bookstore.dtos.CategoriaDTO;
 import com.bribsz.bookstore.entities.Categoria;
@@ -32,5 +37,18 @@ public class CategoriaController {
 		List<Categoria> list = categoriaService.findAll();
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
+		obj = categoriaService.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoriaDTO> update(@PathVariable Integer id, @RequestBody CategoriaDTO objDTO) {
+		Categoria newObj = categoriaService.update(id, objDTO);
+		return ResponseEntity.ok().body(new CategoriaDTO(newObj));
 	}
 }
--- src/main/java/com/bribsz/bookstore/service/CategoriaService.java
+++ src/main/java/com/bribsz/bookstore/service/CategoriaService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bribsz.bookstore.dtos.CategoriaDTO;
 import com.bribsz.bookstore.entities.Categoria;
 import com.bribsz.bookstore.repositories.CategoriaRepository;
 import com.bribsz.bookstore.service.exceptions.ObjectNotFoundException;
@@ -24,5 +25,17 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
+	}
+	
+	public Categoria create(Categoria obj) {
+		obj.setId(null);
+		return categoriaRepository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDTO) {
+		Categoria obj = findById(id);
+		obj.setNome(objDTO.getNome());
+		obj.setDescricao(objDTO.getDescricao());
+		return categoriaRepository.save(obj);
 	}
 }
