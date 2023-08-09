6de17e1a5488bddbb0ed61cb9d7e78c6bf09c6ab
bribsz <brenohribeiroh@gmail.com>
2023-08-09 13:54:02 -0300

    categoria-delete criada

--- bookstore-frontend/src/app/app-routing.module.ts
+++ bookstore-frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/view/home/home.component';
 import { CategoriaReadComponent } from './components/view/categoria/categoria-read/categoria-read.component';
 import { CategoriaCreateComponent } from './components/view/categoria/categoria-create/categoria-create.component';
+import { CategoriaDeleteComponent } from './components/view/categoria/categoria-delete/categoria-delete.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'categorias/create',
     component: CategoriaCreateComponent
+  },
+  {
+    path: 'categorias/delete/:id',
+    component: CategoriaDeleteComponent
   }
 ];
 
